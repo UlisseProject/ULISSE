df5ae422d303419ebf3496611c13a056de0e82b0
ga1i13o <gabriele.trivigno@studenti.it>
2021-03-31 23:46:16 +0200

    completed PV class

--- ulissw/phys_models/PV.py
+++ ulissw/phys_models/PV.py
@@ -6,21 +6,33 @@ import numpy as np
 CONSTANTS = {
     'n_air' : 1.0002926,
     'n_glass' : 1.58992,
-    'NOCT' = 50.
+    'noct' : 50.0, # Nominal operating cell temp
+    'tc_ref' : 25., # Temp ref for the cell
+    'g_ref' : 1000., # Irradiance ref
+    'g_noct' : 800., # Irradiance at noct
+    't_noct' : 20.,
+    'n_diode' : 1.0134, # diode ideality factor  
+    'k_boltz' : 8.6173324e-5
 }
 
 
 class PV:
     BASE_URL = 'https://api.solcast.com.au/world_radiation/forecasts.json'
-    API_KEY = 'YOUR_API_KEY'
-    def __init__(self, l=None, k=None, albedo=None, beta=30, gamma=0):
+    API_KEY = 'hhC6JCzib4-MqjfG48DI0eDSPWT2P9Ez'
+    def __init__(self, l=None, k=None, albedo=None, beta=30, gamma=0, 
+                 n_series=None, i_sc_ref=None, v_oc_ref=None, t_v_oc=None, t_i_sc=None):
         self.l = l
         self.k = k
         self.albedo = albedo
         self.data = None
         self.beta = np.radians(beta); 
         self.gamma = np.radians(gamma);
-    
+        self.n_series = n_series
+        self.i_sc_ref = i_sc_ref
+        self.v_oc_ref = v_oc_ref
+        self.t_v_oc = t_v_oc
+        self.t_i_sc = t_i_sc
+        
     def __query_api(self, lat, long, time_window):
         PARAMS = {'latitude' : lat,
                   'longitude' : long,
@@ -38,8 +50,7 @@ class PV:
         
         return tau
     
-    def __calc_irradiance(self, lat=45.464211, long=9.191383, 
-                          time_window=2, data=None):
+    def __calc_irradiance(self, data=None):
         alt_sol = np.array([np.radians(90-point["zenith"]) for point in data])
         alpha = np.array([np.radians(point["azimuth"]-180) for point in data])
 
@@ -57,12 +68,37 @@ class PV:
         
         tau_b = self.__calc_tau(theta, theta_r)
         tau_d = self.__calc_tau(theta_equiv_diff, theta_r_equiv_diff)
-        
+        # maybe add g_ref
         g = tau_b*g_dni*np.cos(theta) + tau_d*g_dhi*(1+np.cos(self.beta))/2 + g_ref*(1-np.cos(self.beta))/2
 
-        return g
+        return g, g_tot
     
-    def calc_power(self):
-        data = self.__query_api(lat, long, time_window)
+    def calc_power(self, lat=45.464211, long=9.191383, time_window=2):
+        #data = self.__query_api(lat, long, time_window)
+        data = dati['forecasts'][-4:]
         
-        g = self.__calc_irradiance(data=data)
+        g, g_tot = self.__calc_irradiance(data=data)
+        # NOCT formula for cell temp
+        t_amb = np.array([point["air_temp"] for point in data]) 
+        t_amb_ref = CONSTANTS['tc_ref'] - (CONSTANTS['noct'] - CONSTANTS['t_noct'])*CONSTANTS['g_ref']/CONSTANTS['g_noct']
+        
+        tc = t_amb + (CONSTANTS['noct'] - CONSTANTS['t_noct'])*g_tot/CONSTANTS['g_noct'] 
+        
+        # thermal voltage
+        vt = CONSTANTS['n_diode']*CONSTANTS['k_boltz']*(t_amb+273.15)*self.n_series
+        vt_ref = CONSTANTS['n_diode']*CONSTANTS['k_boltz']*(t_amb_ref+273.15)*self.n_series
+        
+        i0_ref = self.i_sc_ref/(np.exp(self.v_oc_ref/(self.n_series*vt_ref)) - 1)
+        
+        tc_diff = tc - CONSTANTS['tc_ref']
+        i_pv = (g_tot/CONSTANTS['g_ref'])*self.i_sc_ref*(1 + self.t_i_sc*(tc_diff))
+        v_oc = self.v_oc_ref*(1 + self.t_v_oc*(tc_diff)) + (self.n_series*vt*CONSTANTS['n_diode'])*np.log(g_tot/CONSTANTS['g_ref'])
+        
+        i0 = i_pv/(np.exp(v_oc/(self.n_series*vt)) - 1)
+        
+        V = np.linspace(0, v_oc, 1000, axis=0)
+        I = i_pv - i0*(np.exp(V/(self.n_series*vt)) - 1)
+        P = I*V        
+        p_out = np.max(P, axis=0)
+        
+        return p_out, I, V
